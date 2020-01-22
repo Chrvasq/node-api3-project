1f57e5292892e7bbad3940a8161f614c924f42b4
Chris Vasquez <chrvasq@outlook.com>
2020-01-21 20:26:21 -0800

    Add postRouter endpoints and middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,76 @@
-const express = require('express');
+const express = require("express");
+const Post = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  Post.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      console.log("Error", err);
+      res.status(500).json({ message: "Exception:", err });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", validatePostId, (req, res) => {
+  res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", validatePostId, (req, res) => {
+  const { id } = req.post;
+  const post = req.post;
+
+  Post.remove(id)
+    .then(() => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      console.log("Error: ", err);
+      res.status(500).json({ message: "Exception: ", err });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put("/:id", validatePostId, (req, res) => {
+  const { id } = req.post;
+  const changes = req.body;
+  const { text } = changes;
+
+  text
+    ? Post.update(id, changes)
+        .then(() => {
+          Post.getById(id)
+            .then(post => {
+              res.status(200).json(post);
+            })
+            .catch(err => {
+              console.log("Error:", err);
+              res.status(500).json({ message: "Exception: ", err });
+            });
+        })
+        .catch(err => {
+          console.log("Error: ", err);
+          res.status(500).json({ message: "Exception: ", err });
+        })
+    : res.status(400).json({ errorMessage: "Must provide text." });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+
+  Post.getById(id)
+    .then(post => {
+      post
+        ? ((req.post = post), next())
+        : res.status(404).json({ message: "Post does not exist." });
+    })
+    .catch(err => {
+      console.log("Error: ", err);
+      res.status(500).json({ message: "Exception:", err });
+    });
 }
 
 module.exports = router;
